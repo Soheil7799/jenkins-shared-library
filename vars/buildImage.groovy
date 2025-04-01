1e9b8191ad080b6318880464de426703912433b4
Mostafa Shirvani <133644950+Soheil7799@users.noreply.github.com>
2025-04-01 12:27:53 +0200

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,8 +2,8 @@
 def call() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+        sh 'docker build -t soheiliam/demo-jenkins:sh-1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push soheiliam/demo-jenkins:sh-1.0'
     }
 }
